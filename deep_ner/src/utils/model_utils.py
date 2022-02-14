a36481c67fb3dd2410aedce6028648eb198b79cb
jinyu.li <jinyu.li@aispeech.com>
2022-02-14 20:34:24 +0800

    增加注释

--- deep_ner/src/utils/model_utils.py
+++ deep_ner/src/utils/model_utils.py
@@ -499,6 +499,110 @@ class MRCModel(BaseModel):
 
         return out
 
+# baseline
+class CRF_MATRIX_Model(BaseModel):
+    def __init__(self,
+                 bert_dir,
+                 num_tags,
+                 dropout_prob=0.1,
+                 **kwargs):
+        super(CRF_MATRIX_Model, self).__init__(bert_dir=bert_dir, dropout_prob=dropout_prob)
+
+        out_dims = self.bert_config.hidden_size
+
+        mid_linear_dims = kwargs.pop('mid_linear_dims', 128)
+
+        self.mid_linear = nn.Sequential(
+            nn.Linear(out_dims, mid_linear_dims),
+            nn.ReLU(),
+            nn.Dropout(dropout_prob)
+        )
+
+        out_dims = mid_linear_dims
+
+        self.classifier = nn.Linear(out_dims, num_tags)
+
+        #将一个不可训练的类型Tensor转换成可以训练的类型parameter并将这个parameter绑定到这个module里面(net.parameter()中就有这个绑定的parameter，所以在参数优化的时候可以进行优化的)
+        self.loss_weight = nn.Parameter(torch.FloatTensor(1), requires_grad=True)# torch.FloatTensor(1), requires_grad=True 只是将参数变成可训练的，并没有绑定在module的parameter列表中。
+        self.loss_weight.data.fill_(-0.2)
+
+        self.crf_module = CRF(num_tags=num_tags, batch_first=True)
+
+        init_blocks = [self.mid_linear, self.classifier]
+
+        self._init_weicrfghts(init_blocks, initializer_range=self.bert_config.initializer_range)
+
+    def forward(self,
+                token_ids,
+                attention_masks,
+                token_type_ids,
+                labels=None,
+                pseudo=None):
+
+        bert_outputs = self.bert_module(
+            input_ids=token_ids,
+            attention_mask=attention_masks,
+            token_type_ids=token_type_ids
+        )
+
+        # 常规
+        seq_out = bert_outputs[0]
+
+        batch_size, max_len, feat_dim = seq_out.shape
+        h_i_matrix = seq_out.view(batch_size, max_len, 1, feat_dim).repeat(1,1,max_len,1)
+        # transpose交换两个维度
+        h_j_matrix = h_i_matrix.transpose(1,2)
+        #最后一个维度拼接
+        span_matrix = torch.cat([h_i_matrix, h_j_matrix], dim=-1)
+        ner_logits = self.dense2(torch.relu(self.dense1(span_matrix)))
+        scores = torch.softmax(ner_logits, dim=-1)
+        logits = torch.argmax(scores, axis=-1)
+
+
+        seq_out = self.mid_linear(seq_out)
+
+        emissions = self.classifier(seq_out)
+
+        if labels is not None:
+            if pseudo is not None:
+                # (batch,)
+                tokens_loss = -1. * self.crf_module(emissions=emissions,
+                                                    tags=labels.long(),
+                                                    mask=attention_masks.byte(),
+                                                    reduction='none')
+
+                # nums of pseudo data
+                pseudo_nums = pseudo.sum().item()
+                total_nums = token_ids.shape[0]
+
+                # learning parameter
+                rate = torch.sigmoid(self.loss_weight)#将值映射到0-1之间
+                if pseudo_nums == 0:
+                    loss_0 = tokens_loss.mean()
+                    loss_1 = (rate*pseudo*tokens_loss).sum()
+                else:
+                    if total_nums == pseudo_nums:
+                        loss_0 = 0
+                    else:
+                        loss_0 = ((1 - rate) * (1 - pseudo) * tokens_loss).sum() / (total_nums - pseudo_nums)
+                    loss_1 = (rate*pseudo*tokens_loss).sum() / pseudo_nums
+
+                tokens_loss = loss_0 + loss_1
+
+            else:
+                tokens_loss = -1. * self.crf_module(emissions=emissions,
+                                                    tags=labels.long(),
+                                                    mask=attention_masks.byte(),
+                                                    reduction='mean')
+
+            out = (tokens_loss,)
+
+        else:
+            tokens_out = self.crf_module.decode(emissions=emissions, mask=attention_masks.byte())
+
+            out = (tokens_out, emissions)
+
+        return out
 
 class EnsembleCRFModel:
     def __init__(self, model_path_list, bert_dir_list, num_tags, device, lamb=1/3):
